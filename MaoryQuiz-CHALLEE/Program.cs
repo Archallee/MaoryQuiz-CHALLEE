d4f6650033035ad377c35859ae6b02f68a47f9f0
Challee Alarcon <ac151535@avcol.school.nz>
2022-11-21 16:47:55 +1300

    addded the rest of the questions

--- MaoryQuiz-CHALLEE/Program.cs
+++ MaoryQuiz-CHALLEE/Program.cs
@@ -101,6 +101,126 @@ if (answer4 == answer[2])
 }
 else
 {
-    point -= 1;
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("5. What animal is a Hoiho?\nA. GOAT\nB. HORSE\nC. PIGEON\nD. CAT");
+Console.WriteLine("Answer: ");
+answer5 = Console.ReadLine();
+while (answer5 != answer[0] && answer5 != answer[1] && answer5 != answer[2] && answer5 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer5 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer5 == answer[1])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("6. Moana greets Mōrena to her neighbours everyday at 7am. What is Mōrena?\nA. GOODBYE\nB. MORE PLEASE\nC. STAY THERE\nD. GOOD MORNING");
+Console.WriteLine("Answer: ");
+answer6 = Console.ReadLine();
+while (answer6 != answer[0] && answer6 != answer[1] && answer6 != answer[2] && answer6 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer6 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer6 == answer[3])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("7. I'm hungry. can we get some kai? What is kai?\nA. CLOTHES\nB. FOOD\nC. TOYS\nD. SLEEP");
+Console.WriteLine("Answer: ");
+answer7 = Console.ReadLine();
+while (answer7 != answer[0] && answer7 != answer[1] && answer7 != answer[2] && answer7 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer7 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer7 == answer[1])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("8. The color of the flamingo is māwhero. What is māwhero?\nA. RED\nB. GREEN\nC. PINK\nD. YELLOW");
+Console.WriteLine("Answer: ");
+answer8 = Console.ReadLine();
+while (answer8 != answer[0] && answer8 != answer[1] && answer8 != answer[2] && answer8 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer8 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer8 == answer[2])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("9. What animal is a poaka?\nA. DUCK\nB. MOUSE\nC. TIGER\nD. PIG");
+Console.WriteLine("Answer: ");
+answer9 = Console.ReadLine();
+while (answer9 != answer[0] && answer9 != answer[1] && answer9 != answer[2] && answer9 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer9 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer9 == answer[3])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
+    Console.WriteLine("Incorrect");
+}
+Console.WriteLine("10. The color of her hair is like wood, it's Parauri. What is Parauri?\nA. BLACK\nB. GREY\nC. BROWN\nD. GREEN");
+Console.WriteLine("Answer: ");
+answer10 = Console.ReadLine();
+while (answer10 != answer[0] && answer10 != answer[1] && answer10 != answer[2] && answer10 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices. Make sure that your answer is in upper case.");
+    answer10 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer10 == answer[2])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point += 0;
     Console.WriteLine("Incorrect");
 }
