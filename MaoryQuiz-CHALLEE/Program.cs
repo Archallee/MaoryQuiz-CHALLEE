04089c1408295655c16a41d3b3960c0ff55976af
Challee Alarcon <ac151535@avcol.school.nz>
2022-11-21 16:35:28 +1300

    added while, if and else

--- MaoryQuiz-CHALLEE/Program.cs
+++ MaoryQuiz-CHALLEE/Program.cs
@@ -47,3 +47,20 @@ else
 Console.WriteLine("2. E tū and e noho are...\nA. JUMP AND DANCE\nB. RUN AND WALK\nC. CLAP AND LAUGH\nD. STAND UP AND SIT DOWN");
 Console.WriteLine("Answer: ");
 answer2 = Console.ReadLine();
+while (answer1 != answer[0] && answer1 != answer[1] && answer1 != answer[2] && answer1 != answer[3])
+{
+    Console.WriteLine("Please write a valid answer from the choices");
+    answer1 = Console.ReadLine();
+}
+Console.Clear();
+
+if (answer2 == answer[3])
+{
+    point += 1;
+    Console.WriteLine("Correct");
+}
+else
+{
+    point -= 1;
+    Console.WriteLine("Incorrect");
+}
